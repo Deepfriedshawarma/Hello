e03e6bfec30edec9be3f8109afff5166243bb91f
Toldr <87395852+Deepfriedshawarma@users.noreply.github.com>
2021-11-06 19:48:38 +0300

    f

--- pages/hello.js
+++ pages/hello.js
@@ -1,3 +1,3 @@
 let answer = prompt('Привет, как тебя зовут?');
-alert(answer + ' давай расскажешь мне как твои дела в телеграмме?');
-alert('Вот ссылка https://t.me/Saito134');
+alert(answer + ', давай расскажешь мне как твои дела в телеграмме?');
+document.write('Вот ссылка' https://t.me/Saito134);
